47170f22be7b0956b01590a42e5a8516b3a2c551
AjinyaNaik <ajinkya0210@gmail.com>
2026-08-11 20:24:49 -0400

    Updaed docker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,19 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import sequelize from "./config/databse";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from the TypeScript Express backend!");
+});
 
 async function startServer() {
   try {
@@ -17,7 +21,7 @@ async function startServer() {
 
     console.log("Database connected successfully.");
 
-    app.listen(port, () => {
+    app.listen(port, "0.0.0.0", () => {
       console.log(`Server running on port ${port}`);
     });
   } catch (error) {
@@ -27,11 +31,3 @@ async function startServer() {
 }
 
 startServer();
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from the TypeScript Express backend!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
