5610ae682427cd7111b7804ef400401906db45af
AjinyaNaik <ajinkya0210@gmail.com>
2026-08-13 11:11:10 -0400

    Revised models for consistency and type safety

--- backend/src/models/SamplePack.model.ts
+++ backend/src/models/SamplePack.model.ts
@@ -1,12 +1,32 @@
-import { Model, DataTypes } from "sequelize";
+import { Model, DataTypes, Optional } from "sequelize";
 import sequelize from "../config/database";
 
-class SamplePack extends Model {
+interface SamplePackAttributes {
+  id: number;
+  name: string;
+  description: string | null;
+  cover_image: string | null;
+  created_at: Date;
+  updated_at: Date;
+}
+
+interface SamplePackCreationAttributes
+  extends Optional<
+    SamplePackAttributes,
+    "id" | "description" | "cover_image" | "created_at" | "updated_at"
+  > {}
+
+class SamplePack
+  extends Model<
+    SamplePackAttributes,
+    SamplePackCreationAttributes
+  >
+  implements SamplePackAttributes
+{
   public id!: number;
   public name!: string;
   public description!: string | null;
   public cover_image!: string | null;
-  public is_selling!: boolean; 
 
   public readonly created_at!: Date;
   public readonly updated_at!: Date;
@@ -19,28 +39,28 @@ SamplePack.init(
       autoIncrement: true,
       primaryKey: true,
     },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
     description: {
       type: DataTypes.TEXT,
       allowNull: true,
     },
+
     cover_image: {
       type: DataTypes.STRING,
       allowNull: true,
     },
-    is_selling: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    },
+
     created_at: {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
+
     updated_at: {
       type: DataTypes.DATE,
       allowNull: false,
