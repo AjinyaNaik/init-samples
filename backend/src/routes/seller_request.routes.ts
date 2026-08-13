9d1b83155e5699f74d7f38bbea18b98e5450e717
AjinyaNaik <ajinkya0210@gmail.com>
2026-08-13 15:34:48 -0400

    Added APIS for user and admin to Create, read and update status

--- backend/src/routes/seller_request.routes.ts
+++ backend/src/routes/seller_request.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { createSellerRequest } from "../controller/seller_request.controller";
-import { authMiddleware } from "../middleware/auth.middleware";
+import { cancelMyRequest, createSellerRequest, getMyRequests, getAllSellerRequestsController, approveSellerRequestController, rejectSellerRequestController} from "../controller/seller_request.controller";
+import { authMiddleware} from "../middleware/auth.middleware";
+import { adminMiddleware } from "../middleware/admin.middleware";
 
 const router = Router();
 
@@ -8,6 +9,41 @@ router.post(
   "/",
   authMiddleware,
   createSellerRequest
+);
+
+router.get(
+  "/my",
+  authMiddleware,
+  getMyRequests
+);
+
+router.delete(
+  "/:id",
+  authMiddleware,
+  cancelMyRequest
+);
+
+// Admin routes
+
+router.get(
+  "/admin",
+  authMiddleware,
+  adminMiddleware,
+  getAllSellerRequestsController
+);
+
+router.patch(
+  "/:id/approve",
+  authMiddleware,
+  adminMiddleware,
+  approveSellerRequestController
+);
+
+router.patch(
+  "/:id/reject",
+  authMiddleware,
+  adminMiddleware,
+  rejectSellerRequestController
 );
 
 export default router;
